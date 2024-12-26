7a46ff2108a7568d737e09af477eec458660317d
pavel barovsky <pbarovsky@mail.ru>
2024-12-26 12:51:28 +0300

    add sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import { useTheme } from "./hooks/useTheme";
+import useSound from "use-sound";
 import GamePage from "./ui/organisms/GamePage";
 import GameOverModal from "./ui/organisms/GameOverModal";
 import StartPage from "./ui/organisms/StartPage";
-import './App.css'
+import "./App.css";
 
-import DARK from "./assets/icons/MOON.svg";
-import LIGHT from './assets/icons/SUN.svg'
+import MODE from "./assets/icons/MODE.svg";
+import SOUND_ON from "./assets/icons/sound_on.svg";
+import SOUND_OFF from "./assets/icons/sound_off.svg";
+import MUSIC from "./assets/sounds/sound.mp3";
 
 const App = () => {
   const { theme, setTheme } = useTheme();
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { stop }] = useSound(MUSIC, {loop: true});
+
+  const toggleSound = () => {
+    if (isPlaying) {
+      stop();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  };
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -31,18 +46,26 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <img
-        src={theme === "light" ? LIGHT : DARK}
-        alt="theme switcher"
-        className="theme_switcher"
-        onClick={toggleTheme}
-      />
+    <>
+      <div className="togglers">
+        <img
+          src={MODE}
+          alt="theme switcher"
+          className="theme_switcher"
+          onClick={toggleTheme}
+        />
+        <img
+          src={isPlaying ? SOUND_ON : SOUND_OFF}
+          alt="Sound toggle"
+          onClick={() => toggleSound()}
+          className="sound_switcher"
+        />
+      </div>
 
       {!isGameStarted && <StartPage onStartGame={startGame} />}
       {isGameStarted && <GamePage onGameOver={endGame} exit={restartGame} />}
       {isGameOver && <GameOverModal onRestart={restartGame} />}
-    </div>
+    </>
   );
 };
 
