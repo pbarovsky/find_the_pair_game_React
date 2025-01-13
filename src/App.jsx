669549a720a54a80ae198cd3583d60a6d9d83aee
pavel barovsky <pbarovsky@mail.ru>
2025-01-13 14:42:50 +0300

    small fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,10 @@
 import { useState } from "react";
-import { useTheme } from "./hooks/useTheme";
-import useSound from "use-sound";
 import GamePage from "./ui/organisms/GamePage";
 import GameOverModal from "./ui/organisms/GameOverModal";
 import StartPage from "./ui/organisms/StartPage";
-import "./App.css";
-
-import MODE from "./assets/icons/MODE.svg";
-import SOUND_ON from "./assets/icons/sound_on.svg";
-import SOUND_OFF from "./assets/icons/sound_off.svg";
-import MUSIC from "./assets/sounds/sound.mp3";
+import EffectTogglers from "./ui/molecules/EffectTogglers";
 
 const App = () => {
-  const { theme, setTheme } = useTheme();
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [play, { stop }] = useSound(MUSIC, {loop: true});
-
-  const toggleSound = () => {
-    if (isPlaying) {
-      stop();
-      setIsPlaying(false);
-    } else {
-      play();
-      setIsPlaying(true);
-    }
-  };
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
-
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
 
@@ -47,21 +22,7 @@ const App = () => {
 
   return (
     <>
-      <div className="togglers">
-        <img
-          src={MODE}
-          alt="theme switcher"
-          className="theme_switcher"
-          onClick={toggleTheme}
-        />
-        <img
-          src={isPlaying ? SOUND_ON : SOUND_OFF}
-          alt="Sound toggle"
-          onClick={() => toggleSound()}
-          className="sound_switcher"
-        />
-      </div>
-
+      <EffectTogglers />
       {!isGameStarted && <StartPage onStartGame={startGame} />}
       {isGameStarted && <GamePage onGameOver={endGame} exit={restartGame} />}
       {isGameOver && <GameOverModal onRestart={restartGame} />}
---
+++ src/ui/molecules/EffectTogglers.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import useSound from "use-sound";
+import { useTheme } from "../../hooks/useTheme";
+import MUSIC from "../../assets/sounds/sound.mp3";
+import sc from "./EffectTogglers.module.css";
+
+import MODE from "../../assets/icons/MODE.svg";
+import SOUND_ON from "../../assets/icons/sound_on.svg";
+import SOUND_OFF from "../../assets/icons/sound_off.svg";
+
+const EffectTogglers = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [play, { stop }] = useSound(MUSIC, { loop: true });
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  const toggleSound = () => {
+    if (isPlaying) {
+      stop();
+      setIsPlaying(false);
+    } else {
+      play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className={sc.togglers}>
+      <img
+        src={MODE}
+        alt="theme switcher"
+        className={sc.theme_switcher}
+        onClick={toggleTheme}
+      />
+      <img
+        src={isPlaying ? SOUND_ON : SOUND_OFF}
+        alt="Sound toggle"
+        onClick={() => toggleSound()}
+        className={sc.sound_switcher}
+      />
+    </div>
+  );
+};
+
+export default EffectTogglers;
