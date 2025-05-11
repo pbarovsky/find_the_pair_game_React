0dc5438036dced687990036dc4321837b06a1fc0
Pavel Barovsky <pbarovsky@mail.ru>
2025-05-11 21:51:35 +0300

    rename proj

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
+    postcss: "./postcss.config.js",
   },
-  base: '/CDG_task_2_slabs_game/',
-})
+  base: "/find_the_pair_game_React/",
+});
